d58549a57c14d1b6f74f40c0a4aa2a9b834fef9f
Arbt7399 <1536864449@qq.com>
2025-05-05 21:06:09 +0800

    5.5 loading2

--- scripts/main.js
+++ scripts/main.js
@@ -75,9 +75,8 @@ let continueWidth, continueTop, continueLeft, goHeight, goWidth, scAniWidth
 let maxScore1, maxScore2, currentScore1, currentScore2, scAniHeight, scAniFont1
 let keyFrames, timing, animation, scAniOutline, keyFrames2, timing2, keyFrames3
 
-let loadingWidth, loadingHeight, loadingTop, loadingTextLeft, loadingFont
+let loadingTop, loadingFont
 const loadingContainer = document.querySelector('.loadingContainer')
-const loadingImg = document.querySelector('.loadingImg')
 const loadingText = document.querySelector('.loadingText')
 
 //按屏幕比例缩放
@@ -296,8 +295,6 @@ function resize() {
   key.style.left = keyboardLeft + 'px'
 
   //loading
-  loadingHeight = 137 / 659 * windowHeight
-  loadingWidth = 330 / 659 * windowHeight
   loadingTop = 160 / 659 * windowHeight
   loadingFont = 17 / 659 * windowHeight
 
@@ -305,12 +302,6 @@ function resize() {
   loadingContainer.style.width = windowWidth + 'px'
   loadingContainer.style.top = 0
   loadingContainer.style.left = 0
-
-  loadingImg.style.height = loadingHeight + 'px'
-  loadingImg.style.width = loadingWidth + 'px'
-  loadingImg.style.top = loadingTop + 'px'
-  loadingImg.style.left = (windowWidth - loadingWidth) / 2 + 'px'
-  loadingImg.style.backgroundSize = loadingWidth + 'px ' + loadingHeight + 'px'
 
   loadingText.style.top = loadingTop + 145 / 659 * windowHeight + 'px'
   loadingText.style.left = windowWidth / 2 - 60 / 659 * windowHeight + 'px'
